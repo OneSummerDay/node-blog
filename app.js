939e61dd9bfda98f0802ce7afa121fe8afc2a7ac
OneSummerDay <artem.kraievyi@gmail.com>
2025-12-07 21:29:53 +0200

    feat: add full project styling

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-require('.env').config();
+require('dotenv').config();
 
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
 
 const app = express();
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 
