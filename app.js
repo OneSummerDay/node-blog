33df2540ebe2545fe5369e078092205f4520bd8f
OneSummerDay <artem.kraievyi@gmail.com>
2025-12-09 20:10:38 +0200

    feat: integrate database connection into server

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ require('dotenv').config();
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
 
+const connectDB = require('./server/config/db');
+
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000 || process.env.PORT;
 
 app.use(express.static('public'));
 
