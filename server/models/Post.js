3765b38a204fba1fc260d3910521d521748a8f37
OneSummerDay <artem.kraievyi@gmail.com>
2025-12-09 20:29:14 +0200

    feat: create Post model schema and set up models directory

---
+++ server/models/Post.js
@@ -0,0 +1,24 @@
+const { default: mongoose } = require('mongoose');
+const mogoose = require('mongoose');
+
+const Schema = mogoose.Schema;
+const PostSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    body: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = mongoose.model('Post', PostSchema);
