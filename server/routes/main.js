cd8af42ccec89ad8c07370504157a0ea8284061f
OneSummerDay <artem.kraievyi@gmail.com>
2025-12-11 20:31:37 +0200

    feat: add GET route to fetch post by id

--- server/routes/main.js
+++ server/routes/main.js
@@ -19,6 +19,25 @@ router.get('', async (req, res) => {
 });
 
 
+router.get('/post/:id', async (req, res) => {
+    try {
+
+        const locals = {
+        title: 'Node.js Blog',
+        description: 'A simple blog with Node.js, express and mongo.db',
+        };
+
+        const slug = req.params.id;
+
+        const data = await Post.findById({ _id: slug });
+        res.render('post', { locals, data });
+    } catch (error) {
+        console.log(error);
+    }
+   
+});
+
+
 router.get('/about', (req, res) => {
     res.render('about');
 });
