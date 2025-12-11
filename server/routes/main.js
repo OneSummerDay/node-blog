be61111fb8cb416525e26d99201fc11c63de871a
OneSummerDay <artem.kraievyi@gmail.com>
2025-12-11 19:15:10 +0200

    feat: add GET route to fetch all posts

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../models/Post');
 
 
-router.get('', (req, res) => {
+router.get('', async (req, res) => {
     const locals = {
         title: 'Node.js Blog',
         description: 'A simple blog with Node.js, express and mongo.db',
     };
 
-
-    res.render('index', locals);
+    try {
+        const data = await Post.find();
+        res.render('index', { locals, data });
+    } catch (error) {
+        console.log(error);
+    }
+   
 });
+
 
 router.get('/about', (req, res) => {
     res.render('about');
